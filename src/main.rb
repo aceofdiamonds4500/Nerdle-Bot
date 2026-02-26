7b0c5f20c67692d62b8fb6a4265d9aaf32743391
aceofdiamonds4500 <tacoparrot4500@gmail.com>
2026-02-26 16:58:44 -0500

    Add comments, add game loop

--- src/main.rb
+++ src/main.rb
@@ -12,37 +12,39 @@ bot = Discordrb::Commands::CommandBot.new token: ENV['DISCORD_BOT_TOKEN'], prefi
 $wordlist_filepath = File.join(__dir__,"../text/wordle-allowed-guesses.db")
 
 bot.message(with_text: 'nerd ping') do |event|
-  # Respond in the same channel
+  # Ping
   event.respond 'nerd pong'
 end
 
-bot.command :say do |_event, *args|
-  args.join(' ')
-end
-
-bot.command :annoy_brian do |_event|
-  _event.respond "ANNOY BRIAN ACTIVATE"
-  _event.respond "@Brian"
-end
+=begin
+Guess today/yesterday/tomorrow all grab the daily Wordle relative to what day it currently is.
+Essentially, the bot wants to get the answer in order to actually compare the answer to something. If you want
+  to modify the code so that it simply sends the answer to the chat, be my guest LMAO.
+Call !guess_(specify day) to get the bot to attempt to solve the Wordle.
+=end
 
 bot.command :guess_today do |_event|
   today = Date.today
   answer = wordle_grab(today)
-  prolog_query(answer)
+  _event << play_game(answer)
 end
 
 bot.command :guess_yesterday do |_event|
   yesterday = Date.today - 1
   answer = wordle_grab(yesterday)
-  prolog_query(answer)
+  _event << play_game(answer)
 end
 
 bot.command :guess_tomorrow do |_event|
   tomorrow = Date.today + 1
   answer = wordle_grab(tomorrow)
-  prolog_query(answer)
+  _event << play_game(answer)
 end
 
+=begin
+Grabs the current URL where the day's Wordle is stored.
+Returns the solution of the puzzle
+=end
 def wordle_grab(day)
   url_str = "https://www.nytimes.com/svc/wordle/v2/#{day}.json"
   uri = URI(url_str)
@@ -51,11 +53,14 @@ def wordle_grab(day)
   result["solution"]
 end
 
+=begin
+Helper function to automatically pick a random 5-letter word to guess.
+=end
 def rand_guess
   seed = Random.new_seed
   rand_num = Random.new(seed)
   guess_num = rand_num.rand(1..10657)
-  random_guess = grab_word(guess_num)
+  grab_word(guess_num)
 end
 
 def grab_word(index=10657)
@@ -63,19 +68,47 @@ def grab_word(index=10657)
   db.execute("SELECT * FROM words WHERE rowid = ?", index).first.first
 end
 
-def prolog_query(pre_answer)
-  guess = rand_guess.chars
-  guess.reverse.reduce([]) { |acc, el| [el, acc] }
-  answer = pre_answer.chars
+=begin
+Main game loop.
+Gets fed the answer from one of the three commands and loops guess queries until it runs out of chances.
+Intended return is the best guess prepended by "I tried []", but still testing other things
+=end
+def play_game(answer_str)
+  answer = answer_str.chars
   answer.reverse.reduce([]) { |acc, el| [el, acc] }
+  compared = nil
+
+  (1..6).each { |i|
+    puts "YIPEEEEE #{i}"
+    guess = rand_guess.chars
+    guess.reverse.reduce([]) { |acc, el| [el, acc] }
+    compared = prolog_query answer, guess
+    if compared == %w[g g g g g]
+      break
+    end
+  }
+  "I tried #{compared}"
+end
+
+=begin
+Contains the entire ruleset for determining if a guess is correct.
+Currently tinkering with it to learn PROLOG in the first place.
+=end
+def prolog_query(answer, guess)
   c = RubyProlog::Core.new
   c.instance_eval do
-    likes['John', guess].fact
-    likes[guess, 'John'].fact
+    likes[answer, guess].fact
+    likes[guess, answer].fact
+    mylist[['g','y','y','g','y']].fact
 
     friends[:X, :Y] << [likes[:X,:Y], likes[:Y,:X]]
+    #^test code to make sure ruby-prolog works^
+
+    #string_comp[:X,:Y] <<
+    #  []
   end
-  c.query { friends['John', :Friends]}
+  #c.query { friends[guess, :Answer]}
+  c.query{mylist[:X]}
 end
 
 # Run the bot
